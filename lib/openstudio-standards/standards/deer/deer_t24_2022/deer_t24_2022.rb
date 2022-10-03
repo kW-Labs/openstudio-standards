cfeb66caa9f5c403a337e57944397bf6db50ffef
Eric Ringold <ericringold@gmail.com>
2022-10-03 16:08:38 -0600

    initial standard class definition

---
+++ lib/openstudio-standards/standards/deer/deer_t24_2022/deer_t24_2022.rb
@@ -0,0 +1,20 @@
+# This class holds methods that apply to DEER T24 2022
+# to a given model.
+# @ref [Refrences::DEERT24]
+class DEERT242022 < DEER
+  register_stanadard 'DEER T24 2022'
+  attr_reader :templates
+
+  def initialize
+    @template = 'DEER T24 2022'
+    load_standards_database
+  end
+
+  # Loads the openstudio standards dataset for this standard.
+  #
+  # @param data_directories [Array<String>] array of file paths that contain standards data
+  # @return [Hash] a hash of standards data
+  def load_standards_database(data_directories = [])
+    super([__dir__] + data_directories)
+  end
+end
